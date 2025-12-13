5157652edca3f4faf2d962a43308e61fef8a8ad1
Arjay Garabiag <garabiag.arjay04@gmail.com>
2025-12-13 18:28:12 +0800

    php mailer

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '795195b6faceb371b8d4c50e41ecbe94a7fb61b1',
+        'reference' => '7565b8e47fe1b4527c53f82cc871cdea1792537e',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '795195b6faceb371b8d4c50e41ecbe94a7fb61b1',
+            'reference' => '7565b8e47fe1b4527c53f82cc871cdea1792537e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
